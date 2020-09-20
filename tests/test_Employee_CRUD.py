5a5abf697c73e59c554c529226724c40e93a3836
Prasad Salpekar <salpekarprasad@gmail.com>
2020-09-20 22:15:24 +0530

    Updated CRUD Test and Update Employee Test

--- tests/test_Employee_CRUD.py
+++ tests/test_Employee_CRUD.py
@@ -1,27 +1,74 @@
 import requests
 import json
+from jsonpath_ng import parse
+import time
+from fake_useragent import UserAgent
 
 
-def test_create_Empl() :
-    req_body = {'name': 'kyle pogi',
-                'salary': '3232323',
-                'age': '34'}
+def create_New_Employee(fixturefilepath):
+    headers = {"Content-Type": "application/json", "Accept": "*/*","Content-Length": "42", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
+    f = open(fixturefilepath, "r")
+    create_request = json.loads(f.read())
+    return requests.post("http://dummy.restapiexample.com/api/v1/create", create_request,headers=headers)
 
-    response = requests.post('http://dummy.restapiexample.com/api/v1/create', req_body)
-    print(response.status_code)
-    print(response.text)
+def get_Employee(empId,emptyUserAgent = False):
+    base_url = "http://dummy.restapiexample.com/api/v1/employee/"
+    user_agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
+    print(user_agent)
+    header = {"Content-Type": "application/json", "Accept": "*/*"}
+    if emptyUserAgent == False :
+        header.update(user_agent)
 
-    # id_exp = parse("$.id")
-    # id = id_exp.find(response.json())
-   # print (id[0].value)
+    get_employee_url = base_url if empId == None else base_url+empId
+    return requests.get(get_employee_url,headers=header)
 
-def test_get_All_Empl() :
+def delete_Employee(empId,emptyUserAgent = False):
+    base_url = "http://dummy.restapiexample.com/api/v1/delete/"
+    user_agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
+    header = {"Content-Type": "application/json", "Accept": "*/*"}
+    if emptyUserAgent == False:
+        header.update(user_agent)
 
-    get_all_employees = "http://dummy.restapiexample.com/api/v1/employees"
-    res = requests.get(get_all_employees)
-    assert res.status_code == 200
+    delete_employee_url = base_url if empId == None else base_url+empId
+    return requests.delete(delete_employee_url,headers = header)
 
-def test_Empl_by_Empl_No() :
-    get_employee_by_EmpNo = "http://dummy.restapiexample.com/api/v1/employee/15"
-    res = requests.get(get_employee_by_EmpNo)
-    #assert res.status_code == 200
+def update_Employee(fixturefilepath,empId,emptyUserAgent = False) :
+    base_url = "http://dummy.restapiexample.com/api/v1/update/"
+    user_agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
+    header = {"Content-Type": "application/json", "Accept": "*/*"}
+    if emptyUserAgent == False:
+        header.update(user_agent)
+
+    update_employee_url = base_url if empId == None else base_url + empId
+    f = open(fixturefilepath, "r")
+    create_request = json.loads(f.read())
+    return requests.put(update_employee_url,create_request,headers = header)
+
+def test_Delete_Newly_Created_Employee():
+    time.sleep(1)
+
+    create_response = create_New_Employee("Fixtures/createEmployeeDetails.json")
+    assert create_response.status_code == 200
+    empl_id = parse("$.data.id")
+    id = empl_id.find(create_response.json())
+
+    get_response_before_update = get_Employee(str(id[0].value))
+    assert get_response_before_update.status_code == 200
+
+    update_response = update_Employee("Fixtures/updateEmployeeDetails.json",str(id[0].value))
+    assert  update_response.status_code == 200
+
+    get_response_after_update_before_delete = get_Employee(str(id[0].value))
+    assert get_response_after_update_before_delete.status_code == 200
+
+    delete_response = delete_Employee(str(id[0].value))
+    assert delete_response.status_code == 200
+
+    get_response_after_delete = get_Employee(str(id[0].value))
+    assert get_response_after_delete.status_code == 200
+
+
+
+
+
+
--- tests/test_Update_Employee.py
+++ tests/test_Update_Employee.py
@@ -1,2 +1,54 @@
 # The API is not working as expected, I tried running the API using POSTMAN, IT shows success in PUT request but when
 # we try to GET the same record it show the old data and not the updated one
+
+
+import requests
+import json
+from jsonpath_ng import parse
+import time
+from fake_useragent import UserAgent
+
+
+
+def get_Employee(empId,emptyUserAgent = False):
+    base_url = "http://dummy.restapiexample.com/api/v1/employee/"
+    user_agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
+    print(user_agent)
+    header = {"Content-Type": "application/json", "Accept": "*/*"}
+    if emptyUserAgent == False :
+        header.update(user_agent)
+
+    get_employee_url = base_url if empId == None else base_url+empId
+    return requests.get(get_employee_url,headers=header)
+
+
+def update_Employee(fixturefilepath,empId,emptyUserAgent = False) :
+    base_url = "http://dummy.restapiexample.com/api/v1/update/"
+    user_agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
+    header = {"Content-Type": "application/json", "Accept": "*/*"}
+    if emptyUserAgent == False:
+        header.update(user_agent)
+
+    update_employee_url = base_url if empId == None else base_url + empId
+    f = open(fixturefilepath, "r")
+    create_request = json.loads(f.read())
+    return requests.put(update_employee_url,create_request,headers = header)
+
+def test_Update_Existing_Employee_Details():
+    time.sleep(1)
+
+    get_response_before_update = get_Employee(str(id[0].value))
+    assert get_response_before_update.status_code == 200
+
+    update_response = update_Employee("Fixtures/updateEmployeeDetails.json",str(id[0].value))
+    assert  update_response.status_code == 200
+
+    get_response_after_update_before_delete = get_Employee(str(id[0].value))
+    assert get_response_after_update_before_delete.status_code == 200
+
+
+
+
+
+
+
